78e286b5ce40dfad54ad5d30ab7190d5d9fc6e3f
Sohan Patil <patil.sohan@northeastern.edu>
2024-10-08 16:03:16 -0400

    validate status success check

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_1_cidr
