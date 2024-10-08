d981b420cd1fd61756961e67a6885264f4a04f88
Sohan Patil <patil.sohan@northeastern.edu>
2024-10-08 02:09:54 -0400

    Added subnets

--- main.tf
+++ main.tf
@@ -6,3 +6,72 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_1_cidr
+  availability_zone       = var.availability_zones[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1"
+  }
+
+  depends_on = [aws_vpc.main]
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = var.availability_zones[1]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-2"
+  }
+
+  depends_on = [aws_vpc.main]
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_3_cidr
+  availability_zone       = var.availability_zones[2]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-3"
+  }
+
+  depends_on = [aws_vpc.main]
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_1_cidr
+  availability_zone = var.availability_zones[0]
+  tags = {
+    Name = "private-subnet-1"
+  }
+
+  depends_on = [aws_vpc.main]
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_2_cidr
+  availability_zone = var.availability_zones[1]
+  tags = {
+    Name = "private-subnet-2"
+  }
+
+  depends_on = [aws_vpc.main]
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_3_cidr
+  availability_zone = var.availability_zones[2]
+  tags = {
+    Name = "private-subnet-3"
+  }
+
+  depends_on = [aws_vpc.main]
+}
